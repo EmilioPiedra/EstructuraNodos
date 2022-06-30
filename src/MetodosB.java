81678b8b448bb49d87c320bb2289dc8a74caddb8
Emilio Piedra León <espiedra1@utpl.edu.ec>
2022-06-29 22:19:22 -0500

    recupercion

--- src/MetodosB.java
+++ src/MetodosB.java
@@ -13,20 +13,23 @@ public class MetodosB {
 
     public int menu (){
         int op=0;
-        System.out.println("Ingresar un nuevo elemento 1");
-        System.out.println("Recorrer la lista 2");
-        System.out.println("");
-        System.out.println("salir 0");
+        System.out.println("Ingresar un nuevo elemento   [1]");
+        System.out.println("Recorrer la lista            [2]");
+        System.out.println("Eliminar nodo                [3] ");
+        System.out.println("Salir                        [0]");
         op =leer.nextInt();
      return op;
     }
 
+    //este metodo es para saber si la lista esta vacia poque el ejercicio es mas complejo porque
+    // lleva un ordenamiento de las lista
     public boolean listaVacia (){
         if (head==null)
             return true;
             else
             return false;
     }
+   //este metodo es para ingresar y crear un nodo en la lista
     public void ingresar (){
         int id,edad,n1,n2,n3;
         String nombre;
@@ -43,26 +46,34 @@ public class MetodosB {
         n2 = leer.nextInt();
         System.out.println("Ingrese nota 3: ");
         n3 = leer.nextInt();
-
+        //una vez leido los datos se los lleva al nodo que tiene todos los parametros de persona y se tiene una relacion de
+        // tipo composion porque es directamente proporcional esto lleva.
         Nodo nuevo = new Nodo(id,nombre,edad,n1,n2,n3);
-        /*if (listaVacia()){
+        //caso 1 para el id de los alumnos.
+        // este caso es para el ordenamiento de la lista de nodos
+        if (listaVacia()){
             head = nuevo;
             tail = nuevo;
-
+            //en esta sentencia el ordenamiento es directo
+            // ya que si es dato ingresado es menor al que ya esta en la lista se movera al inicio( head)
+            // dando como resultado el ordenamieto correcto de la lista.
+        }else  if (id <head.alumno.id){
+            nuevo.sig = head;
+            head = nuevo;
         }else {
-            tail.sig = nuevo;
-            tail = nuevo;
-        } */
-
-        nuevo.sig = nuevo;
-        if (listaVacia()){
-            tail = nuevo;
-        }else {
-
+            //crea un nodo auxiliar de tipo nodo por si es mayor al cabeza entonces se deberia empezar desde cabeza
+            Nodo actual = head;
+            while ((actual.sig != null)&&(id>actual.sig.alumno.id))
+                actual = actual.sig;
+            nuevo.sig = actual.sig;
+            actual.sig = nuevo;
+            if (nuevo.sig == null)
+                tail=nuevo;
         }
-
     }
     public void recorrer (){
+        // se podira crea un nodo de tipo nodo que esta situado en la cabeza de lista (head)
+        // y se rrecorrera toda la lista que ya esta ordenada anteriormente en el metodo de ordenar.
         Nodo actual = head;
         float prom;
         while (actual != null){
@@ -72,7 +83,30 @@ public class MetodosB {
             prom=((actual.alumno.notas[0]+actual.alumno.notas[1]+actual.alumno.notas[2])/actual.alumno.notas.length);
             System.out.println("Promedio : "+prom);
             actual =actual.sig;
+            System.out.println("---------------------------------------------");
+        }
 
+    }
+    public void eliminar (){
+        int id=0;
+        System.out.println("Ingresar id: ");
+        id = leer.nextInt();
+        if (listaVacia()){
+            System.out.println("La lista esta vacia...");
+        }else if (id == head.alumno.id){
+            head = head.sig;
+            if (listaVacia())
+                tail=null;
+        }else{
+            Nodo actual = head;
+            while ((actual.sig!= null)&&(id !=actual.sig.alumno.id))
+                actual = actual.sig;
+            if (actual.sig != null)
+                System.out.println(id+"No esta en la lista");
+            else
+                actual.sig = actual.sig.sig;
+            if (actual.sig ==null)
+                tail=actual;
         }
     }
 
